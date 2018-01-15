e40081ff6ef2e1b82aacc7646e214b91ce693cab
ilhampraset <ilhampraset30@gmail.com>
2018-01-15 16:28:37 +0700

    added gentelella

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+Route::get('test', function(){
+	return view('dashboard.index');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
